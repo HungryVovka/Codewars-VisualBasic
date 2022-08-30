904197ba8b949a4a135d876804e2f2f4a2649a95
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-31 02:47:06 +0500

    feat: 8 kyu solutions

--- 8kyu/Multiply.vb
+++ 8kyu/Multiply.vb
@@ -1,4 +1,6 @@
+' -----------------------------------------------------------
 ' This code does not execute properly. Try to figure out why.
+' -----------------------------------------------------------
 
 Public Module Example
   Public Function Multiply(ByVal a As Integer, ByVal b As Integer) As Integer
