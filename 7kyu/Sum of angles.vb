1865c3a890cd1ea1107f18fdfcca8c368b41c3c0
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-19 23:28:47 +0500

    fix: 7 kyu solutions

--- 7kyu/Sum of angles.vb
+++
@@ -1,9 +0,0 @@
-' -----------------------------------------------------------
-' Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
-' -----------------------------------------------------------
-
-Public Module Kata
-    Public Function Angle(ByVal n As Integer) As Integer
-        Return (n - 2) * 180
-    End Function
-End Module
