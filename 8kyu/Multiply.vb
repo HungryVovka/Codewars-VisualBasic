c15430d9f2e446a7a4b2d87ac0bce98cd8a08657
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-11-19 23:26:13 +0500

    fix: 8 kyu solutions

--- 8kyu/Multiply.vb
+++
@@ -1,9 +0,0 @@
-' -----------------------------------------------------------
-' This code does not execute properly. Try to figure out why.
-' -----------------------------------------------------------
-
-Public Module Example
-  Public Function Multiply(ByVal a As Integer, ByVal b As Integer) As Integer
-    Return a * b
-  End Function
-End Module
